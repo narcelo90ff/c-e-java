f7e20f6a2270ff9f5c98c34eadd2a5e6cd585f20
narcelo90ff <marcelonassar700@gmail.com>
2026-04-17 19:34:08 -0300

    Update Curso.cs

--- atividade2deC#/Models/Curso.cs
+++ atividade2deC#/Models/Curso.cs
@@ -5,9 +5,7 @@ public class Curso
     public int Id { get; set; }
     public string Nome { get; set; } = string.Empty;
 
-    // Chave estrangeira
     public int ProfessorId { get; set; }
 
-    // Propriedade de navegação
     public Professor Professor { get; set; } = null!;
 }
