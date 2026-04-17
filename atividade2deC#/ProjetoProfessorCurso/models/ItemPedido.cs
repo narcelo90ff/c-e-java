95523dd3bdd82d1b901eebd4849538c15dfd8768
narcelo90ff <marcelonassar700@gmail.com>
2026-04-17 19:34:35 -0300

    Update ItemPedido.cs

--- atividade2deC#/ProjetoProfessorCurso/models/ItemPedido.cs
+++ atividade2deC#/ProjetoProfessorCurso/models/ItemPedido.cs
@@ -6,12 +6,9 @@ public class ItemPedido
     public string Produto { get; set; } = string.Empty;
     public int Quantidade { get; set; }
 
-    // ✅ Adicionado preço unitário — faz sentido em qualquer pedido real
     public decimal PrecoUnitario { get; set; }
 
-    // Chave estrangeira
     public int PedidoId { get; set; }
 
-    // Propriedade de navegação
     public Pedido Pedido { get; set; } = null!;
 }
