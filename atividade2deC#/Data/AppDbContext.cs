1d2cb168ad03a352f3e05b09ac760499b605c603
narcelo90ff <marcelonassar700@gmail.com>
2026-04-17 11:55:44 -0300

    Update AppDbContext.cs

---
+++ atividade2deC#/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoProfessorCurso.Models;
+
+namespace ProjetoProfessorCurso.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Professor> Professores { get; set; }
+    public DbSet<Curso> Cursos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=escola.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configura o relacionamento 1:N explicitamente
+        modelBuilder.Entity<Curso>()
+            .HasOne(c => c.Professor)
+            .WithMany(p => p.Cursos)
+            .HasForeignKey(c => c.ProfessorId);
+    }
+}
